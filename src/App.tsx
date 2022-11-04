5700984d2aa225add8f1f2c4ae8c68c68b825159
Patrick Bellamy <patrckbell@gmail.com>
2022-11-04 23:59:20 +1000

    spinny logo wooooo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
+import { motion } from "framer-motion";
 
 function App() {
-  return (<div className='App'>  <img src="../LOGO2.png"></img> </div>);
+  return (<motion.div
+    className="container"
+    whileHover={{ scale: 1.2, rotate: 180}}
+    whileTap={{ scale: 0.8, rotate: -180, borderRadius: "100%" }}>
+      <img src="../LOGO2.png"></img>
+    </motion.div>);
 }
 
 export default App;
