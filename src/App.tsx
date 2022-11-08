b6b7b1b66548b4cbd6e84beb33e5a9ec56fb696f
Patrick Bellamy <patrckbell@gmail.com>
2022-11-08 16:27:59 +1000

    logo expand on website open' '

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import { motion } from "framer-motion";
 function App() {
   return (<motion.div
     className="container"
+    initial={{ opacity: 0, scale: 0 }}
+    animate={{ opacity: 1, scale: 1 }}
+    transition={{ duration: 0.3 }}
     whileHover={{ scale: 1.2, rotate: 180}}
     whileTap={{ scale: 0.8, rotate: -180, borderRadius: "100%" }}>
       <img src="../LOGO2.png"></img>
